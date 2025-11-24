cd9168674d04ae28732c0c7b3c661479b03eb315
dayvisonmsilva <dayvisoneryc@exemplo.com>
2025-11-23 22:26:11 -0300

    Estrutura inicial: Backend Django configurado com Custom User e pastas separadas

---
+++ backend/core/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class Usuario(AbstractUser):
+    # O AbstractUser já tem: username, first_name, last_name, email, password
+    cpf = models.CharField(max_length=14, unique=True)
+    matricula = models.CharField(max_length=20, unique=True)
+
+    # Resolve conflitos de herança do Django (obrigatório ao extender AbstractUser)
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        related_name="custom_usuario_set",
+        related_query_name="user",
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name="custom_usuario_set",
+        related_query_name="user",
+    )
+
+    class Meta:
+        verbose_name = 'Usuário'
