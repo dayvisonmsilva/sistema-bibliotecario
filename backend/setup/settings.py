af4be70f3f4dca3550f92ec84d2488af621d1322
dayvisonmsilva <dayvisoneryc@exemplo.com>
2025-12-01 02:05:02 -0300

    fix(docs): configura suporte a JWT Bearer no Swagger UI

--- backend/setup/settings.py
+++ backend/setup/settings.py
@@ -141,3 +141,13 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+        }
+    }
+}
