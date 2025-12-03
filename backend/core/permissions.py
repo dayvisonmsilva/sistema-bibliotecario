721af5904442a3727817d693d04a4ea65c6a57e7
dayvisonmsilva <dayvisoneryc@exemplo.com>
2025-12-03 19:44:31 -0300

    fix(permissions): permite acesso de superusuários em rotas de bibliotecário

--- backend/core/permissions.py
+++ backend/core/permissions.py
@@ -2,4 +2,4 @@ from rest_framework import permissions
 
 class IsBibliotecario(permissions.BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_authenticated and hasattr(request.user, 'bibliotecario'))
+        return bool(request.user and request.user.is_authenticated and (request.user.is_superuser or hasattr(request.user, 'bibliotecario')))
